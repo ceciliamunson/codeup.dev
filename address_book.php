d36a6f03ab2254b46ed302b9e0fa1ac925915d8b
ceciliamunson <ceci_ugarte@hotmail.com>
2014-02-28 09:56:12 -0600

    Added an include() to get your new address_data_store.php file.

--- address_book.php
+++ address_book.php
@@ -2,38 +2,11 @@
 
 //var_dump($_POST);
 
+$address_book = [];
 $fields = [];
 
+include('address_data_store.php');
 
-class AddressDataStore {
-
-    public $filename = '';
-
-    function __construct($filename = '')
-    {
-        $this->filename = $filename;
-    }
-
-	function write_address_book($address_book) {
-	    
-	    $handle = fopen($this->filename, 'w+');
-	    foreach ($address_book as $fields) {
-			fputcsv($handle, $fields);
-		}
-		fclose($handle);
-	}
-	
-	function read_address_book() {
-	
-		$address_book = [];
-		$handle = fopen($this->filename, 'r');
-		while (($data = fgetcsv($handle)) !== FALSE) {
-	  		$address_book[] = $data;
-		}
-		fclose($handle);
-		return $address_book;
-	}
-}
 $book = new AddressDataStore('address_book.csv');
 $address_book = $book->read_address_book();
 
