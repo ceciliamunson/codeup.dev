d30f23e2c584ca33578413e504b05bb1f0df25d7
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-03 16:00:41 -0600

    Adde constructor to override parent constructor & convert all filenames to lowercase

--- address_data_store.php
+++ address_data_store.php
@@ -1,32 +1,37 @@
 <?php
 
-class AddressDataStore {
+require_once('filestore.php');
 
-    public $filename = '';
+class AddressDataStore extends Filestore {
 
-    function __construct($filename = '')
-    {
-        $this->filename = $filename;
-    }
 
-	function write_address_book($address_book) {
-	    
-	    $handle = fopen($this->filename, 'w+');
-	    foreach ($address_book as $fields) {
-			fputcsv($handle, $fields);
-		}
-		fclose($handle);
+    function __construct($filename) {
+    
+    	$filename = strtolower($filename);
+    	parent::__construct($filename);
 	}
-	
-	function read_address_book() {
-	
-		$handle = fopen($this->filename, 'r');
-		while (($data = fgetcsv($handle)) !== FALSE) {
-	  		$address_book[] = $data;
-		}
-		fclose($handle);
-		return $address_book;
-	}
+
 }
+
+
+	// function write_address_book($address_book) {
+	    
+	//     $handle = fopen($this->filename, 'w+');
+	//     foreach ($address_book as $fields) {
+	// 		fputcsv($handle, $fields);
+	// 	}
+	// 	fclose($handle);
+	// }
+	
+	// function read_address_book() {
+	// 	$address_book = [];
+	// 	$handle = fopen($this->filename, 'r');
+	// 	while (($data = fgetcsv($handle)) !== FALSE) {
+	//   		$address_book[] = $data;
+	// 	}
+	// 	fclose($handle);
+	// 	return $address_book;
+	// }
+
 
 ?>
