1990e92bed9f5172d7e0bfb2376bfec13c7de5e4
ceciliamunson <ceci_ugarte@hotmail.com>
2014-03-17 14:42:37 -0500

    Adde form that allows national parks to be added to database. Used prepared statements for all queries that contain user input. Validate all form inputs to ensure that database entries are not empty.

--- national_parks.php
+++ national_parks.php
@@ -5,36 +5,112 @@ $mysqli = @new mysqli('127.0.0.1', 'codeup', 'password', 'codeup_mysqli_test_db'
 // Check for errors
 if ($mysqli->connect_errno) {
     echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
-}
-else {
 
-    echo $mysqli->host_info . "\n";
 }
 
-$sortColumn = $_GET['sort_column'];
-$sortOrder = $_GET['sort_order'];
+
+if (!empty($_GET)) {
+    $sortColumn = $_GET['sort_column'];
+    $sortOrder = $_GET['sort_order'];
+    // Retrieve a result set using SELECT
+    $result = $mysqli->query("SELECT * FROM national_parks ORDER BY $sortColumn $sortOrder");  
+}
+
+
+if (!empty($_POST)) {
+    try {
+        if (empty($_POST['name'])) {
+            throw new Exception("Field can't be empty!  ");
+        } 
+        elseif (empty($_POST['location'])) {
+            throw new Exception("Field can't be empty!  ");
+        }
+        elseif (empty($_POST['description'])) {
+            throw new Exception("Field can't be empty!  ");
+        }
+        elseif (empty($_POST['date_est'])) {
+            throw new Exception("Field can't be empty!  ");
+        }
+        elseif (empty($_POST['area_acres'])) {
+            throw new Exception("Field can't be empty!  ");
+        }
+        else {
+            // Create the prepared statement
+            $stmt = $mysqli->prepare("INSERT INTO national_parks (name, location, description, date_est, area_acres) VALUES (?, ?, ?, ?, ?)");
             
-// Retrieve a result set using SELECT
-$result = $mysqli->query("SELECT * FROM national_parks ORDER BY $sortColumn $sortOrder");
+            // bind parameters
+            $stmt->bind_param("sssss", $_POST['name'], $_POST['location'], $_POST['description'], $_POST['date_est'], $_POST['area_acres']);
+            
+            // execute query, return result
+            $stmt->execute();
+        }
+    } 
+    catch (Exception $e) {
+    
+        $errorMessage = $e->getMessage();
+    }
+}
 
+$result = $mysqli->query("SELECT * FROM national_parks");              
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Top 10 National Parks</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <h1>Top 10 National Parks</h1>
+<!-- Button trigger modal -->
+<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Add park
+</button>
+<?php if (!empty($errorMessage)) { ?>
+    <div class="alert alert-danger"><?= $errorMessage ?></div>
+<?php } ?>
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <form method="POST" role="form" action="national_parks.php">
+          <div class="form-group">
+            <label for="name"></label>
+            <input type="text" name="name" class="form-control" id="name" placeholder="Park name">
+          </div>
+          <div class="form-group">
+            <label for="location"></label>
+            <input type="text" name="location" class="form-control" id="loacation" placeholder="Location">
+          </div>
+          <div class="form-group">
+            <label for="description"></label>
+            <textarea type="text" name="description" class="form-control" id="description" placeholder="Description"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="date_est"></label>
+            <input type="text" name="date_est" class="form-control" id="date_est" placeholder="Date Established">
+          </div>
+          <div class="form-group">
+            <label for="area_acres"></label>
+            <input type="text" name="area_acres" class="form-control" id="area_acres" placeholder="Area in acres">
+          </div>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
-    <!-- <p>
-        <a href="?sort_column=foo&amp;sort_order=desc">Click to sort desc</a>
-    </p> -->
-    <?php if (!empty($_GET)) : ?>
-        We are sorting <?= $_GET['sort_column'] . ' ' . $_GET['sort_order']; ?>
-    <?php endif; ?>
     <table>
         <tr>
             <th><a href="?sort_column=name&amp;sort_order=asc">Name</a>&nbsp<a href="?sort_column=name&amp;sort_order=desc"><span class="glyphicon glyphicon-sort-by-alphabet-alt"></span></a></th>
@@ -57,7 +133,8 @@ $result = $mysqli->query("SELECT * FROM national_parks ORDER BY $sortColumn $sor
 
 
     </table>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 <html>
 
